2317cc81e1f2d8b6685cc9502c8929b888830fe9
Anni-ui <annikabachen@gmail.com>
2026-04-16 23:20:24 +0200

    first commit

---
+++ Spast.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+x, y = 400, 300
+color = (255, 107, 53)
+red = [60, 110, 155]
+green = [155, 60, 107]
+blue = [107, 155, 60]
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:  x -= 5
+    if keys[pygame.K_RIGHT]: x += 5
+    if keys[pygame.K_UP]:    y -= 5
+    if keys[pygame.K_DOWN]:  y += 5
+    if keys[pygame.K_SPACE]: color = (random.choice(red), random.choice(green), random.choice(blue))
+
+    screen.fill((11, 13, 16))
+    pygame.draw.rect(screen, (color), (x, y, 40, 40))
+    pygame.draw.rect(screen, (color), (700, 500, 40, 40))
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
