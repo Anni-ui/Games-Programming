11a6233e36e312e9e52c4ecc98eb268ebecd9bdc
Anni-ui <annikabachen@gmail.com>
2026-04-20 23:22:57 +0200

    Überarbeitet (Teil1, Beschreibung)
    Ersten Teil der Übung überarbeitet und  Beschreibungen hinzugefügt.

--- Übung001/uebung_001.py
+++ Übung001/uebung_001.py
@@ -1,26 +1,33 @@
 #Variablen deklariert
-Ergebnis = 437
+Summe = 0
+#Variable Ende ist 1 mehr als die letzte Zahl die wir brauchen, da diese vom Code nicht mitgezählt wird.
 Ende = 32483
 Wert1 = 3.1
 Wert2 = 5.4
 
-total = sum(range(Ergebnis, Ende))
-print(total)
+#for Schleife, die alle Zahlen von 437 bis 32482 zusammen rechnet.
+for i in range(437, Ende):
+    Summe = Summe + i
+#print gibt die Summe der Zahlen von 437 bis 32482 in der Konsole aus. 
+print(Summe)    
 
-#for Schleife für die Zahlen von 437 bis 32483
-for Ergebnis in range(Ende):
-    Ergebnis = Ergebnis + Ergebnis + 1
-
-print(Ergebnis)
-
+# if fragt ab, ob der erste Wert größer ist als der zweite. 
 if Wert1 > Wert2: 
+  # falls das im mit if abgefragte zutrifft wird der zweite Wert durch den ersten geteilt.
    result = Wert2 / Wert1 
+   #print gibt das Ergebnis der Variable result in der Konsole aus.
    print(result)
+#elif wird aktiviert, falls das if nicht zutrifft (if = false). Elif fragt ab, ob der erste Wert kleiner ist als der zweite.
 elif Wert1 < Wert2:
+    #falls das im elif abgefragte zutrifft wird der erste Wert durch den zweiten geteilt.
     result = Wert1 / Wert2
+    #print gibt das Ergebnis der Variable result in der Konsole aus.
     print(result)   
+#else wird aktiviert falls das if und das elif nicht zutreffen (beide false sind). 
 else :
+    #dann wird die Variable result auf 1 gesetzt.
     result = 1
+    #print gibt das Ergebnis der Variable result in der Konsole aus.
     print(result)
 
     
