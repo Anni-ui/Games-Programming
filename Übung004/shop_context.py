abe81ae81f03d00be64a8427d69040c9576d8f9d
Anni-ui <annikabachen@gmail.com>
2026-07-08 02:41:25 +0200

    weiteres upgrade hinzugefügt

--- Übung004/shop_context.py
+++ Übung004/shop_context.py
@@ -9,11 +9,12 @@ class ShopContext(Context):
 
     UPGRADES = [
     {"name": "Blades", "rarity": "common", "cost": 1,
-    "effect": {"stat": "dmg", "op": "add", "value": 1}},
+    "effect": {"stat": "dmg", "op": "add", "value": 5}},
     {"name": "Hide", "rarity": "common", "cost": 1,
     "effect": {"stat": "hp_max", "op": "add", "value": 10}},
     {"name": "Berserk", "rarity": "rare", "cost": 5,
     "effect": {"stat": "dmg", "op": "mul", "value": 1.5}},
+    {"name": "Speed", "rarity": "common", "cost": 1, "effect": {"stat": "shotspd", "op": "mul", "value": 1.5}},
     ]
 
 
