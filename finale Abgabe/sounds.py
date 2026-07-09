839dbb5f184c4b6344809a19ff6be55a6cdb4d4e
Anni-ui <annikabachen@gmail.com>
2026-07-09 02:26:16 +0200

    neue Assets + Sounds, Klasse für Sounds, um diese einmal zu laden

---
+++ finale Abgabe/sounds.py
@@ -0,0 +1,13 @@
+# Sounds - Sounds werden hier geladen 
+
+import pygame
+
+class Sounds():
+    buy = pygame.mixer.Sound("buy.wav")
+    decline = pygame.mixer.Sound("decline.wav")
+    enemy_shot = pygame.mixer.Sound("enemy_shot.wav")
+    enemy_dead = pygame.mixer.Sound("enemy_dead.wav")
+    player_shot = pygame.mixer.Sound("player_shot.wav")
+    player_dead = pygame.mixer.Sound("player_dead.wav")    
+    power_up = pygame.mixer.Sound("power_up.wav")
+    qin = pygame.mixer.Sound("win.wav")
